ded39ef1fd7709dc862b3af9d0e94cee25b9bc3c
Paul McArthur <pmcarthur@gmail.com>
2020-08-11 10:24:20 +0100

    Add files via upload
    A small script I wrote for work to help pick out info from a 'master' excel spreadsheet. I use the python module 'pick' - which gives the option in the terminal with a simple GUI. It works quite well for the basic needs of the script. Next I what to amend it of it can loop and ask if you want to pick again .... rather than straight to 'exit'.

---
+++ ticklist_locations.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+
+import pandas as pd
+import numpy as np
+import io
+import xlrd
+import xlsxwriter
+import matplotlib.pyplot as plt
+import csv
+import plotly.express as px
+from pick import pick
+
+df = pd.read_excel(r'Z:\Docs\DailyOperationalProcedures\Password Tick Sheets\Password Change Tick Sheet Master.xlsx', sheet_name='Server List', skiprows=3, usecols='B,C,D,E,H,I')
+
+
+df.columns = df.iloc[0]
+df = df[1:]
+
+#df.columns
+
+
+df = df[pd.notnull(df['Customer'])]
+
+
+unique_cust = df["Customer"].unique()
+
+
+
+title = 'Which Customer to Query? '
+options = ['Fujitsu', 'NI WATER', 'eHR Connect', 'Libraries NI', 'CAFOS', 'EA', 'IMS', 'SSCL', 'Whitbread BART']
+option, index = pick(options, title)
+x = option
+y = index
+print(x)
+
+
+print(df[df['Customer'].str.match(x) | (df.index == 0)])
+
+
+input('\nPress Enter to Exit Screen...')
+
