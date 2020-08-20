e35e81739527258ae69eb27ac2cb2eb098d22217
Paul McArthur <pmcarthur@gmail.com>
2020-08-20 14:27:06 +0100

    Update ticklist_locations.py
    Looping the code back to the beginning using main()

--- ticklist_locations.py
+++ ticklist_locations.py
@@ -1,50 +1,58 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+def main():
+
+    import pandas as pd
+    import numpy as np
+    import io
+    import xlrd
+    import xlsxwriter
+    import matplotlib.pyplot as plt
+    import csv
+    import plotly.express as px
+    from pick import pick
+    import os
+
+
+    cmd = 'mode 180,60' # To set the size of the Terminal Window
+    os.system(cmd)
+
+    #width = "180"
+    #height = "60"
+    #os.system("mode con cols="+width+"lines="+height)
+
+
+    df = pd.read_excel(r'Z:\Docs\DailyOperationalProcedures\Password Tick Sheets\Password Change Tick Sheet Master.xlsx', sheet_name='Server List', skiprows=3, usecols='B,C,D,E,H,I')
+
+
+    df.columns = df.iloc[0]
+    df = df[1:]
+
+    #df.columns
+
+
+    df = df[pd.notnull(df['Customer'])]
+
+
+    unique_cust = df["Customer"].unique()
 
 
 
-
-import pandas as pd
-import numpy as np
-import io
-import xlrd
-import xlsxwriter
-import matplotlib.pyplot as plt
-import csv
-import plotly.express as px
-from pick import pick
-import os
+    title = 'Which Customer to Query? '
+    options = ['Fujitsu', 'NI WATER', 'eHR Connect', 'Libraries NI', 'CAFOS', 'EA', 'IMS', 'SSCL', 'Whitbread BART']
+    option, index = pick(options, title)
+    x = option
+    y = index
+    print(x)
 
 
-cmd = 'mode 180,60' # To set the size of the Terminal Window
-os.system(cmd)
+    print(df[df['Customer'].str.match(x) | (df.index == 0)])
 
-df = pd.read_excel(r'Z:\Docs\DailyOperationalProcedures\Password Tick Sheets\Password Change Tick Sheet Master.xlsx', sheet_name='Server List', skiprows=3, usecols='B,C,D,E,H,I')
+    restart=input("\nDo you wish to pick another option .... yes or just hit 'enter' to exit   ")
+    if restart == "yes":
+        main()
 
+    else:
+            exit()
 
-df.columns = df.iloc[0]
-df = df[1:]
-
-#df.columns
-
-
-df = df[pd.notnull(df['Customer'])]
-
-
-unique_cust = df["Customer"].unique()
-
-
-
-title = 'Which Customer to Query? '
-options = ['Fujitsu', 'NI WATER', 'eHR Connect', 'Libraries NI', 'CAFOS', 'EA', 'IMS', 'SSCL', 'Whitbread BART']
-option, index = pick(options, title)
-x = option
-y = index
-print(x)
-
-
-print(df[df['Customer'].str.match(x) | (df.index == 0)])
-
-
-input('\nPress Enter to Exit Screen...')
+#This is where the code starts again
+main()
 
