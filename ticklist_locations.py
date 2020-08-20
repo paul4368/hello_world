835f73bf8072ebbd3856cc0c2d37154b21cc5f8c
Paul McArthur <pmcarthur@gmail.com>
2020-08-20 11:50:51 +0100

    Update ticklist_locations.py
    using import os .... the following code used to set the size of the 'output' Terminal Window
    
    cmd = 'mode 180,60'
    os.system(cmd)
    
    another way is .....
    
    width = "180"
    height = "60"
    os.system("mode con cols="+width+"lines="+height)

--- ticklist_locations.py
+++ ticklist_locations.py
@@ -13,6 +13,11 @@ import matplotlib.pyplot as plt
 import csv
 import plotly.express as px
 from pick import pick
+import os
+
+
+cmd = 'mode 180,60' # To set the size of the Terminal Window
+os.system(cmd)
 
 df = pd.read_excel(r'Z:\Docs\DailyOperationalProcedures\Password Tick Sheets\Password Change Tick Sheet Master.xlsx', sheet_name='Server List', skiprows=3, usecols='B,C,D,E,H,I')
 
